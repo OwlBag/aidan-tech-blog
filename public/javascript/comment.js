5a4d63fc47eb4c2e0c11090d34a37dbe2d285fd8
OwlBag <Owlbag0410@gmail.com>
2022-06-12 22:37:26 -0400

    add frontend scripts

---
+++ public/javascript/comment.js
@@ -0,0 +1,27 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const comment_body = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString()[window.location.toString().length - 1];
+
+    if (comment_body) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', nreFormHandler);
